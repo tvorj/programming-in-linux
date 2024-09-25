cae9db12eb9dc1752ae4d317ceda3e94034451f8
Anatoly <felixmeister@yandex.ru>
2024-09-25 21:16:37 +0400

    move functions to header file

---
+++ FS-1/ReadFile.hpp
@@ -0,0 +1,26 @@
+#pragma once
+void cat(char* file){
+        char buffer[100];
+        int fd = open(file, O_RDONLY);
+        if(fd == -1){
+                perror("error opening file");
+                exit(EXIT_FAILURE);
+        }
+        ssize_t bytes_read = 0;
+        while(bytes_read < 99){
+                ssize_t temp = read(fd, buffer + bytes_read, 10);
+                bytes_read += temp;
+                if(temp == 0){
+                        break;
+                }
+                if(temp == -1){
+                        perror("error reading file");
+                        close(fd);
+                        exit(EXIT_FAILURE);
+                }
+        }
+        buffer[bytes_read] = '\0';
+        std::cout<<static_cast<std::string>(buffer);
+        close(fd);
+}
+
--- FS-1/main.cpp
+++ FS-1/main.cpp
@@ -4,31 +4,7 @@
 #include <unistd.h>
 #include <cstring>
 #include <string>
-
-void cat(char* file){
-	char buffer[100];
-	int fd = open(file, O_RDONLY);
-        if(fd == -1){
-                perror("error opening file");
-                exit(EXIT_FAILURE);
-	}
-	ssize_t bytes_read = 0;
-        while(bytes_read < 99){
-		ssize_t temp = read(fd, buffer + bytes_read, 10);
-		bytes_read += temp;
-		if(temp == 0){
-			break;
-		}
-		if(temp == -1){
-                	perror("error reading file");
-                	close(fd);
-                	exit(EXIT_FAILURE);
-        }
-        }
-	buffer[bytes_read] = '\0';
-	std::cout<<static_cast<std::string>(buffer);
-	close(fd);
-}
+#include "ReadFile.hpp"
 
 int main(int argc, char* argv[]){
 	for(std::size_t i = 1; i < argc; i++){
